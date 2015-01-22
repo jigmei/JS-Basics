a68f38b9f2df4842d9c3c1d08cc999286e1a0d39
jigmei <jigmeidorjee@Jigmeis-MacBook-Pro.local>
2015-01-21 20:29:25 -0700

    hw

--- practice.js
+++ practice.js
@@ -6,6 +6,13 @@ var name = 'Tyler';
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
   //Code Here
+  var isTheName=function(name){
+if(name==="Tyler"){
+console.log("This is the name");
+}
+else
+console.log("is not the name");
+}
 
 
 //Next problem
@@ -15,7 +22,13 @@ var name = 'Tyler';
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
 
-  //Code Here
+  var getTheName=function(){
+ prompt('name');
+
+}
+
+
+
 
 
 //Next Problem
@@ -26,6 +39,9 @@ var name = 'Tyler';
 //then alerts "Welcome, " plus whatever the users name is.
 
   //Code Here
+  var welcome=function(){
+ return getTheName()
+}
 
 
 //Next problem
@@ -35,8 +51,16 @@ var name = 'Tyler';
 
 //What is the difference between arguments and parameters?
 
-  //Answer Here
+  
+/*Parameter:
 
+A parameter represents a value that the procedure expects you to pass when you call it.
+The procedure's declaration defines its parameters.
+
+Argument:
+
+An argument represents the value you pass to a procedure parameter when you call the procedure.
+The calling code supplies the arguments when it calls the procedure.*/
 
 //Next problem
 
@@ -44,7 +68,7 @@ var name = 'Tyler';
 
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
 
-
+/*false, 0, Null, NaN.*/
   //Answer Here
 
 
@@ -56,25 +80,41 @@ var name = 'Tyler';
 //Create a function called myName that returns your name
 
   //Code Here
+  var myName=function(){
+  	  var name='Jigmei';
+
+  	return (name);
+  }
 
 //Now save the function definition of myName into a new variable called newMyName
+
+	 var newMyName=myName();
+
 
   //Code Here
 
 //Now alert the result of invoking newMyName
 
-
+alert (newMyName);
 
 //Next problem
 
 
 
 //Create a function called outerFn which returns an anonymous function which returns your name.
+var outerFn=function(){
+     var anonymous=function(){
+     	return "jigmei";
+     }
+	return anonymous();
+}
 
   //Code Here
 
 //Now save the result of invoking outerFn into a variable called innerFn.
+var innerFn=outerFn();
 
   //Code Here
 
 //Now invoke innerFn.
+innerFn;
